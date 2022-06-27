5e867fda1a23531465171373292dc383efebf4ba
Himjyoti <himjyotisarma@tutanota.com>
2022-06-27 14:43:22 +0530

    Complete Updated Code with Game completion

--- script.js
+++ script.js
@@ -4,26 +4,51 @@ function computerplay(){
     return myarray[random];
 }
 
+let playerScore=0;
+let computerScore=0;
+
 function playround(playerselection,computerselection){
     if(playerselection==computerselection){
-        return ("It\'s a tie.You both selected "+playerselection);
+        return "tie";
     }else if(playerselection=="rock" && computerselection=="paper"){
-        return ("You lose.Paper beat Rock.")
+        return "lose"
     }else if(playerselection=="rock" && computerselection=="scissor"){
-        return ("You Win.Rock beat Scissor.")
+        return "Win"
     }else if(playerselection=="paper" && computerselection=="rock"){
-        return ("You Win.Paper beat Rock.")
+        return "Win"
     }else if(playerselection=="paper" && computerselection=="scissor"){
-        return ("You lose.Scissor beat Paper.")
+        return "lose"
     }else if(playerselection=="scissor" && computerselection=="rock"){
-        return ("You Lose.Rock beat Scissor.")
+        return "lose"
     }else if(playerselection=="scissor" && computerselection=="paper"){
-        return ("You Win.Scissor beat Paper.")
+        return "Win"
     }else{
-        return ("Make a selection first")
+        return "Make a selection first"
     }
 }
-const firstplayerselection="ROck";
-const playerselection=firstplayerselection.toLowerCase();
-const computerselection=computerplay();
-console.log(playround(playerselection,computerselection));
+
+function game(){
+    for(let i=0;i<5;i++){
+        const firstplayerselection=prompt("Enter anyone of the following: Rock, Paper, Scissor?")
+        const playerselection=firstplayerselection.toLowerCase();
+        const computerselection=computerplay();
+        let result=playround(playerselection,computerselection);
+        if (result=="tie"){
+            console.log("It's a Tie.Both of you have selected "+(playerselection.toUpperCase()))
+        }else if(result=="Win"){
+            playerScore++
+            console.log("You have Won this Round. "+(playerselection.toUpperCase())+" beats "+(computerselection.toUpperCase()))
+        }else if(result=="lose"){
+            computerScore++
+            console.log("Computer have Won this Round. "+(computerselection.toUpperCase())+" beats "+(playerselection.toUpperCase()))
+        }
+    }
+    if(playerScore>computerScore){
+        console.log("Congratulation!!..You have Won this match.Your Score was "+playerScore+" and Computer Score was "+computerScore)  
+    }else if(computerScore>playerScore){
+        console.log("You have Lost this match.Your Score was "+playerScore+" and Computer Score was "+computerScore)  
+    }else{
+        console.log("The Match was a Draw.Your Score "+playerScore+" as well as Computer's Score "+computerScore+" was the same.")
+    }  
+}
+game()
